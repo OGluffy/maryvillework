c0563129aef3e1a3f64ee3bbc87cd0b680bb46e5
OGluffy <68878277+OGluffy@users.noreply.github.com>
2020-09-02 10:08:57 -0500

    Update ball_filler.py

--- ball_filler.py
+++ ball_filler.py
@@ -1,5 +1,8 @@
 import math
-#number of bowling balls to produce (50)
+#number of bowling balls to produce (100)
+print (how many bowling balls will be manyfactured?)
+print (what is the diameter of each ball in inches?)
 diameter(8.5)
+print (what is the coreVolume in inches cubed?)
 coreVolume (124in**3)
 ballfiller = math.pi * 
