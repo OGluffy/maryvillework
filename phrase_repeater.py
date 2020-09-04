c6b0eb09102df111a6052390dafbf6a7b7481a17
OGluffy <68878277+OGluffy@users.noreply.github.com>
2020-09-04 17:57:37 -0500

    Update phrase_repeater.py

--- phrase_repeater.py
+++ phrase_repeater.py
@@ -1,2 +1,5 @@
-for index in range(5):
-    print((index, "I'm bout to mosey"))
+N = input ("Input your phrase: ")
+R = input ("how many times should is be repeated? ")
+conversion = int(R)
+for index in range(1, conversion+1):
+    print (index, N)
