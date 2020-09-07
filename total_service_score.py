20e1fdd9330bc94c54e97f4a764a4abbc7616418
OGluffy <68878277+OGluffy@users.noreply.github.com>
2020-09-07 17:05:52 -0500

    Add files via upload

---
+++ total_service_score.py
@@ -0,0 +1,10 @@
+input ("How many days of scores? ")
+input ("Enter score for day 1: ")
+input ("Enter score for day 2: ")
+input ("Enter score for day 3: ")
+input ("Enter score for day 4: ")
+sum = 0
+list = [2, 4, -2, 1]
+for num in list:
+    sum = sum +num
+print("The total score of the 4 days is",sum)
