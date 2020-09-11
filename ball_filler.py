5e937514cc2083f96e72fb4f2872113b9a538cb6
OGluffy <68878277+OGluffy@users.noreply.github.com>
2020-09-11 12:27:07 -0500

    Update ball_filler.py

--- ball_filler.py
+++ ball_filler.py
@@ -1,5 +1,18 @@
+import math
+
 ballsmanufacturedstring = input ("How many bowling balls will be manufactured? ")
-diameterstring = input ("What is the diameter of each ball in inches? ") 
-volumestring = input ("What is the core volume in inches cubed? ")
-coreVolume = float(input(100 * 197.55))
-print( "You will need", coreVolume, "inches cubed of filler."
+ballsmanu = int(ballsmanufacturedstring)
+
+diameterstring = input ("What is the diameter of each ball in inches? ")
+diame = float(diameterstring)
+
+corevolumestring = input ("What is the core volume in inches cubed? ")
+covolu = int(corevolumestring)
+
+volume_ofa_ball = (4/3) * math.pi * ((diame/2)**3)
+
+filler_per_ball = volume_ofa_ball - covolu
+
+amount_of_filler = ballsmanu * filler_per_ball
+
+print( "You will need", amount_of_filler, "inches cubed of filler."
