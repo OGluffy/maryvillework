347f2947aa58fff99321e5e361a1c4cb030a6bb6
OGluffy <68878277+OGluffy@users.noreply.github.com>
2020-09-13 03:09:37 -0500

    Update total_service_score.py

--- total_service_score.py
+++ total_service_score.py
@@ -1,10 +1,7 @@
-input ("How many days of scores? ")
-input ("Enter score for day 1: ")
-input ("Enter score for day 2: ")
-input ("Enter score for day 3: ")
-input ("Enter score for day 4: ")
-sum = 0
-list = [2, 4, -2, 1]
-for num in list:
-    sum = sum +num
-print("The total score of the 4 days is",sum)
+score_number = input("How many days of scores? ")
+sum_num = 0
+for score in range(int(score_number)):
+    score = input("Enter score for day : ")
+    sum_num += int(score)
+
+print("The total score of the 4 days is", sum_num)
