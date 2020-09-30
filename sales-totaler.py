d68c09bcca30880a6f53a9a97f5c319488b50e11
OGluffy <68878277+OGluffy@users.noreply.github.com>
2020-09-29 21:16:27 -0500

    Update sales-totaler.py

--- sales-totaler.py
+++ sales-totaler.py
@@ -8,15 +8,15 @@ readSales = inputFile.readlines()
 
 for number in readSales:
     splitDollar = numbers.strip('\n').replace('$', '').split(' ')
-    firstSales = float(slpitDollar[0])
+    firstSales = float(splitDollar[0])
     secondSales = float(splitDollar[1])
 
     total = firstSales + secondSales
 
-    order_Total_Message = '${:7.2f} ${:7.2f} ${:7.2f}\n'.format(firstSales, secondSales, total)
+    order_Total_Message = '${:8.2f} ${:8.2f} ${:8.2f}\n'.format(firstSales, secondSales, total)
     outputFile.write(order_Total_Message)
 
 inputFile.close()
 outputFile.close()
 
-print("\n Done writing total to: {0}".format(outputFile))
+print("\n Done writing total to: {0}".format(outputFileName))
