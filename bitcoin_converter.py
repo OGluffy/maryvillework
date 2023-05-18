bac1daf70d68cfb7f2153e96c7d12df653d7f12e
OGluffy <68878277+OGluffy@users.noreply.github.com>
2023-05-18 12:23:58 -0500

    Update bitcoin_converter.py

--- bitcoin_converter.py
+++ bitcoin_converter.py
@@ -1,6 +1,6 @@
 #for converting BTC in USD
 #current BTU rate 11000 per 1$ USD
-exchangeRate = 11000.0
+exchangeRate = 27,034.82
 print ( "As of Aug. 31, 2020, 12:35 AM UTC, Bitcoin is currently trading at {0} per bitcoin.".format(exchangeRate))
 BTC = float(input("enter the BTC amount "))
 currentBTC = BTC * exchangeRate
